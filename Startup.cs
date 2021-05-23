fdc95695041360ab335ae1032df0a80cea634fe5
Киселёв Андрей <vkusnaya.travka@gmail.com>
2021-05-24 00:27:17 +0300

    Done

---
+++ Startup.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KSISLab8 {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapPut("path/to/{file}",
+                async context => {
+                    using (FileStream fs = File.Create($"Content\\{context.GetRouteValue("file")}")) {
+                        await context.Request.Body.CopyToAsync(fs);
+                    }
+                    await context.Response.WriteAsync("OK");
+                });
+
+            routeBuilder.MapGet("path/to/{file}",
+                async context => {
+                    try {
+                        await context.Response.WriteAsync(File.ReadAllText($"Content\\{context.GetRouteValue("file")}"));
+                    }
+                    catch {
+                        context.Response.StatusCode = 404;
+                    }
+                });
+
+            routeBuilder.MapDelete("path/to/{file}",
+                async context => {
+                    FileInfo fileInfo = new FileInfo($"Content\\{context.GetRouteValue("file")}");
+                    if (fileInfo.Exists) {
+                        fileInfo.Delete();
+                    }
+                    else {
+                        context.Response.StatusCode = 404;
+                    }
+                });
+
+            routeBuilder.MapRoute("path/to/{file}",
+                async context => {
+                    try {
+                        FileInfo fileInfo = new FileInfo($"Content\\{context.GetRouteValue("file")}");
+                        if (fileInfo.Exists) {
+                            if (HttpMethods.IsHead(context.Request.Method)) {
+                                context.Response.Headers.Add("File-FullName", fileInfo.FullName);
+                                context.Response.Headers.Add("File-Length", fileInfo.Length.ToString());
+                                context.Response.Headers.Add("File-CreationTime", fileInfo.CreationTime.ToString());
+                            }
+                        }
+                        else {
+                            context.Response.StatusCode = 404;
+                        }
+                    }
+                    catch {
+                        context.Response.StatusCode = 404;
+                    }
+                });
+
+            routeBuilder.MapGet("api/files",
+                async context => {
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(Directory.GetFiles("Content"), Formatting.Indented));
+                });
+
+            app.UseRouter(routeBuilder.Build());
+        }
+    }
+}
